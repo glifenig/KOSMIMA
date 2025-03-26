915d207145e427fde98f68311e17d14c9975dd57
glifenig <161988358+glifenig@users.noreply.github.com>
2025-03-26 16:24:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,20 +11,25 @@ async function fetchProducts() {
         let productHTML = ""; // Store all products before adding to DOM
 
         response.documents.forEach(product => {
-            productHTML += `
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="card mb-4 product-wap rounded-0" style="background-color: whitesmoke;">
-                        <div class="card rounded-0">
-                            <img class="card-img rounded-0 img-fluid" src="${product.image1[0]}" alt="${product.title}">
+            productHTML += `<div class="col-md-4">
+                        <div class="card mb-4 product-wap rounded-0" style="background-color: whitesmoke;">
+                            <div class="card rounded-0">
+                                <img class="card-img rounded-0 img-fluid" src="5.png">
+                                <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                                    
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <a href="shop-single.html" class="h3 text-decoration-none">Oupidatat non</a>
+                                <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                    <li>M/L/X/XL</li>
+
+                                </ul>
+                                
+                                <p class="text-center mb-0">$250.00</p>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <a href="shop-single.html?id=${product.$id}" class="h3 text-decoration-none">${product.title}</a>
-                            <p class="text-muted">${product.shortDescription}</p>
-                            <p class="text-center mb-0">$${product.price}</p>
-                        </div>
-                    </div>
-                </div>
-            `;
+                    </div>`;
         });
 
         productList.innerHTML = productHTML; // Add all products at once
